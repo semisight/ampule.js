7350396ac7da69fec0e5f057699eb69b26395ae9
Alex Bogart <alex@bitlimn.com>
2012-10-23 19:42:46 -0700

    Adds multiple oscillators.
    You can now have more than one waveform per note.

--- ampule.js
+++ ampule.js
@@ -18,39 +18,41 @@ var ampule = function(melody, totalDuration) {
 			note.note = [note.note];
 
 		var duration = note.duration * S/1000;
-		var shape = note.shape || 2;
 		var ar = note.ar || 1;
+
+		note.shape = note.shape || 1;
+		if(!note.shape[0])
+			note.shape = [note.shape];
 
 		for (var samp=currentFloat; samp<(duration + currentFloat); samp++) {
 			genData[samp] = 0;
 
 			for(var n in note.note) {
-				var freq = 440*Math.pow(2, note.note[n]/12);
-				var modSamp = samp%(S/freq)
-				var maxSamp = S/freq;
+				for(var s in note.shape) {
+					var freq = 440*Math.pow(2, note.note[n]/12);
+					var shape = note.shape[s];
+					var modSamp = samp%(S/freq)
+					var maxSamp = S/freq;
 
-				//Render wave from given shape.
-				if(shape == 0)
-					genData[samp] += 0;
-				if(shape == 1)
-					genData[samp] += Math.sin(samp*freq*2*Math.PI / S);
-				if(shape == 2)
-					genData[samp] += 2*(modSamp / maxSamp) - 1;
-				if(shape == 3)
-					genData[samp] += modSamp > maxSamp*.5 ? 1.0 : -1.0;
-				if(shape == 4)
-					genData[samp] += 2*Math.random() - 1;
+					//Render wave from given shape.
+					if(shape == 0)
+						genData[samp] += 0;
+					if(shape == 1)
+						genData[samp] += Math.sin(samp*freq*2*Math.PI / S);
+					if(shape == 2)
+						genData[samp] += 2*(modSamp / maxSamp) - 1;
+					if(shape == 3)
+						genData[samp] += modSamp > maxSamp*.5 ? 1.0 : -1.0;
+					if(shape == 4)
+						genData[samp] += 2*Math.random() - 1;
+				}
 			}
 
 			//Scale wave to fit simple AR envelope
 			genData[samp] *= Math.pow((duration-samp+currentFloat)/duration, ar);
 
-			//Run the wave through a low-pass filter, or, in the case of a drum, a high-pass filter
-			var temp = samp==0 ? genData[samp] : .5*genData[samp] + .5*genData[samp-1];
-			genData[samp] = shape==4 ? genData[samp] - temp : temp;
-
 			//Finally, scale the wave to the appropriate size
-			genData[samp] /= note.note.length;
+			genData[samp] /= (note.note.length * note.shape.length);
 		}
 
 		currentFloat += duration;
