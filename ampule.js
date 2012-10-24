045c1abff78c6a9a9592b8af42546072d5f2868d
Alex Bogart <alex@bitlimn.com>
2012-10-23 19:21:20 -0700

    Adds polyphony.
    We can do more than one key!!

--- ampule.js
+++ ampule.js
@@ -1,12 +1,10 @@
 var ampule = function(melody, totalDuration) {
-	var context, node, S, pointer, buffer, genData, currentFloat;
-
 	//Initialize the variables (build the context, node, etc.)
-	context = new webkitAudioContext();
-	node = context.createJavaScriptNode(1024, 0, 1);
+	var context = new webkitAudioContext();
+	var node = context.createJavaScriptNode(1024, 0, 1);
 	node.onaudioprocess = process;
-	S = context.sampleRate;
-	pointer = currentFloat = 0;
+	var S = context.sampleRate;
+	var pointer = currentFloat = 0;
 
 	buffer = new ArrayBuffer(4 * Math.ceil(totalDuration * S/1000));
 	genData = new Float32Array(buffer);
@@ -14,37 +12,45 @@ var ampule = function(melody, totalDuration) {
 	//Fill the genData buffer with the rendered music.
 	for(var i=0; i<melody.length; i++) {
 		var note = melody[i];
-		var freq = 440*Math.pow(2, note.note/12);
+
+		//Adding polyphony. If note is a single number then make it an array of 1.
+		if(!note.note[0])
+			note.note = [note.note];
+
 		var duration = note.duration * S/1000;
-		var shape = note.shape || 0;
+		var shape = note.shape || 2;
 		var ar = note.ar || 1;
 
 		for (var samp=currentFloat; samp<(duration + currentFloat); samp++) {
-			var modSamp = samp%(S/freq), maxSamp = S/freq;
+			genData[samp] = 0;
 
-			//Render wave from given shape.
-			if(shape == 0)
-				genData[samp] = 0;
-			if(shape == 1)
-				genData[samp] = Math.sin(samp*freq*2*Math.PI / S);
-			if(shape == 2)
-				genData[samp] = 2*(modSamp / maxSamp) - 1;
-			if(shape == 3)
-				genData[samp] = modSamp > maxSamp*.5 ? 1.0 : -1.0;
-			if(shape == 4)
-				genData[samp] = 2*Math.random() - 1;
+			for(var n in note.note) {
+				var freq = 440*Math.pow(2, note.note[n]/12);
+				var modSamp = samp%(S/freq)
+				var maxSamp = S/freq;
+
+				//Render wave from given shape.
+				if(shape == 0)
+					genData[samp] += 0;
+				if(shape == 1)
+					genData[samp] += Math.sin(samp*freq*2*Math.PI / S);
+				if(shape == 2)
+					genData[samp] += 2*(modSamp / maxSamp) - 1;
+				if(shape == 3)
+					genData[samp] += modSamp > maxSamp*.5 ? 1.0 : -1.0;
+				if(shape == 4)
+					genData[samp] += 2*Math.random() - 1;
+			}
 
 			//Scale wave to fit simple AR envelope
 			genData[samp] *= Math.pow((duration-samp+currentFloat)/duration, ar);
 
-			//Run the wave through a low-pass filter
-			//var temp = samp==0 ? genData[samp] : .5*genData[samp] + .5*genData[samp-1];
+			//Run the wave through a low-pass filter, or, in the case of a drum, a high-pass filter
+			var temp = samp==0 ? genData[samp] : .5*genData[samp] + .5*genData[samp-1];
+			genData[samp] = shape==4 ? genData[samp] - temp : temp;
 
-			//or, in the case of a drum, a high-pass filter
-			//genData[samp] = shape==4 ? genData[samp] - temp : temp;
-
-			//optional: convert to "8-bit"
-			//genData[samp] = Math.round(genData[samp]*128)/128;
+			//Finally, scale the wave to the appropriate size
+			genData[samp] /= note.note.length;
 		}
 
 		currentFloat += duration;
@@ -61,6 +67,5 @@ var ampule = function(melody, totalDuration) {
 			node.disconnect();
 	};
 
-	console.log(genData);
 	return function() { node.connect(context.destination); };
 };
